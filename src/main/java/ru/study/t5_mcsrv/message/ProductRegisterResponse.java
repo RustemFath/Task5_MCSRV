ca91171afcf2bfe6e83469130c394af6c9aafe81
Фатхутдинов Рустем Римович <rfathutdinov@dev.vtb.ru>
2024-01-10 17:40:03 +0500

    Change responses

--- src/main/java/ru/study/t5_mcsrv/message/ProductRegisterResponse.java
+++ src/main/java/ru/study/t5_mcsrv/message/ProductRegisterResponse.java
@@ -14,4 +14,27 @@ public class ProductRegisterResponse {
 
     /** Идентификатор продуктового регистра */
     private String accountId;
+
+    public static ProductRegisterResponse getInternalErrorResponse(String errorText) {
+        return getStatusResponse(HttpStatus.INTERNAL_SERVER_ERROR, errorText);
+    }
+
+    public static ProductRegisterResponse getBadResponse(String errorText) {
+        return getStatusResponse(HttpStatus.BAD_REQUEST, errorText);
+    }
+
+    public static ProductRegisterResponse getNotFoundResponse(String errorText) {
+        return getStatusResponse(HttpStatus.NOT_FOUND, errorText);
+    }
+
+    public static ProductRegisterResponse getOkResponse(String accountId) {
+        return getStatusResponse(HttpStatus.OK, accountId);
+    }
+
+    private static ProductRegisterResponse getStatusResponse(HttpStatus status, String accountId) {
+        ProductRegisterResponse response = new ProductRegisterResponse();
+        response.accountId = accountId;
+        response.status = status;
+        return response;
+    }
 }
--- src/main/java/ru/study/t5_mcsrv/message/ProductResponse.java
+++ src/main/java/ru/study/t5_mcsrv/message/ProductResponse.java
@@ -2,10 +2,8 @@ package ru.study.t5_mcsrv.message;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
-import java.io.Serial;
 import java.util.List;
 
 /**
@@ -22,4 +20,27 @@ public class ProductResponse {
     private List<String> registerId;
     /** Массив доп. соглашений */
     private List<String> supplementaryAgreementId;
+
+    public static ProductResponse getInternalErrorResponse(String errorText) {
+        return getStatusResponse(HttpStatus.INTERNAL_SERVER_ERROR, errorText);
+    }
+
+    public static ProductResponse getBadResponse(String errorText) {
+        return getStatusResponse(HttpStatus.BAD_REQUEST, errorText);
+    }
+
+    public static ProductResponse getNotFoundResponse(String errorText) {
+        return getStatusResponse(HttpStatus.NOT_FOUND, errorText);
+    }
+
+    public static ProductResponse getOkResponse(String instanceId) {
+        return getStatusResponse(HttpStatus.OK, instanceId);
+    }
+
+    private static ProductResponse getStatusResponse(HttpStatus status, String instanceId) {
+        ProductResponse response = new ProductResponse();
+        response.setInstanceId(instanceId);
+        response.setStatus(status);
+        return response;
+    }
 }
