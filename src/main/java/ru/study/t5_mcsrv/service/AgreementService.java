fce2049ec341a08dbeb6ab05174e83a59dcfde12
Фатхутдинов Рустем Римович <rfathutdinov@dev.vtb.ru>
2024-01-11 17:35:40 +0500

    Change AgreementService

--- src/main/java/ru/study/t5_mcsrv/service/AgreementService.java
+++ src/main/java/ru/study/t5_mcsrv/service/AgreementService.java
@@ -1,6 +1,6 @@
 package ru.study.t5_mcsrv.service;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.study.t5_mcsrv.entity.Agreement;
@@ -10,14 +10,17 @@ import ru.study.t5_mcsrv.message.ProductResponse;
 import ru.study.t5_mcsrv.repo.AgreementRepository;
 import ru.study.t5_mcsrv.repo.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Сервис, создающий доп. соглашение
+ * Сервис, создающий доп. соглашения
  */
 @Service
 public class AgreementService {
+    @Autowired
     private ProductRepository productRepo;
+    @Autowired
     private AgreementRepository agreementRepo;
 
     @Transactional
@@ -31,7 +34,7 @@ public class AgreementService {
 
         // Проверка таблицы ДС на дубли
         for (var dc : request.getInstanceAgreements()) {
-            List<Agreement> listDC = agreementRepo.findAgreementsByNumber(dc.getNumber());
+            final List<Agreement> listDC = agreementRepo.findAgreementsByNumber(dc.getNumber());
             if (!listDC.isEmpty()) {
                 return ProductResponse.getBadResponse(
                         String.format("Параметр № Дополнительного соглашения (сделки) Number %s уже существует для ЭП с ИД %d",
@@ -39,13 +42,27 @@ public class AgreementService {
             }
         }
 
-        // создать новое доп соглашение
+        // Проверка на дубли номеров во входящем массиве
+        int uniqNumberSize = request.getInstanceAgreements().stream().distinct().toList().size();
+        if (uniqNumberSize < request.getInstanceAgreements().size()) {
+            return ProductResponse.getBadResponse(
+                    String.format("Обнаружены дубли параметра № Дополнительного соглашения (сделки) Number для ЭП с ИД %d",
+                            request.getContractId()));
+        }
 
-        // Запрос у ГС счета и привязка к продуктовому регистру
+        // создать массив новых доп соглашений
+        List<Agreement> agreements = new ArrayList<>(request.getInstanceAgreements().size());
+        for (var dc : request.getInstanceAgreements()) {
+            Agreement agreement = new Agreement();
+            agreement.setProductId(product.getId());
+            agreement.setNumber(dc.getNumber());
+            agreement = agreementRepo.save(agreement);
+            agreements.add(agreement);
+        }
 
-        ProductResponse response = new ProductResponse();
-        response.setInstanceId(product.getId().toString());
-        response.setStatus(HttpStatus.OK);
+        // Формирование ответа
+        ProductResponse response = ProductResponse.getOkResponse(product.getId().toString());
+        response.setSupplementaryAgreementId(agreements.stream().map(agr -> agr.getId().toString()).toList());
         return response;
     }
 }
