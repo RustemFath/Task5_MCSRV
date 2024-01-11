20e3b21226ced14acb635a1427f179f681a24d9b
Фатхутдинов Рустем Римович <rfathutdinov@dev.vtb.ru>
2024-01-11 17:37:46 +0500

    Add tests ProductController

---
+++ src/test/java/ru/study/t5_mcsrv/ProductControllerTest.java
@@ -0,0 +1,92 @@
+package ru.study.t5_mcsrv;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.study.t5_mcsrv.controller.ProductController;
+import ru.study.t5_mcsrv.message.ProductRequest;
+import ru.study.t5_mcsrv.message.ProductResponse;
+import ru.study.t5_mcsrv.service.AgreementService;
+import ru.study.t5_mcsrv.service.ProductService;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ProductControllerTest {
+
+    private final ProductController productController;
+    private final ProductService productServiceMock;
+    private final AgreementService agreementServiceMock;
+
+    public ProductControllerTest() {
+        productController = new ProductController();
+
+        productServiceMock = Mockito.mock(ProductService.class);
+        agreementServiceMock = Mockito.mock(AgreementService.class);
+
+        productController.setProductService(productServiceMock);
+        productController.setAgreementService(agreementServiceMock);
+    }
+
+    @BeforeEach
+    void initEach() {
+        Mockito.reset(productServiceMock, agreementServiceMock);
+    }
+
+    @Test
+    @DisplayName("Ошибка - получение пустого запроса на создание продукта")
+    public void error_emptyRequest() {
+        final ProductRequest request = new ProductRequest();
+
+        Mockito.when(productServiceMock.validateRequest(request))
+                .thenReturn(ProductResponse.getBadResponse("BAD"));
+
+        final ResponseEntity<ProductResponse> response = productController.postRequest(request);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Ошибка - получение null запроса на создание продукта")
+    public void error_nullRequest() {
+        Mockito.when(productServiceMock.validateRequest(null))
+                .thenReturn(ProductResponse.getBadResponse("BAD"));
+
+        final ResponseEntity<ProductResponse> response = productController.postRequest(null);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Успех - создание нового продукта")
+    public void success_createProduct() {
+        final ProductRequest request = new ProductRequest();
+
+        Mockito.when(productServiceMock.validateRequest(request)).thenReturn(null);
+        Mockito.when(productServiceMock.createProduct(request))
+                .thenReturn(ProductResponse.getOkResponse("1"));
+
+        final ResponseEntity<ProductResponse> response = productController.postRequest(request);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Успех - создание нового доп соглашения")
+    public void success_createAgreement() {
+        final ProductRequest request = new ProductRequest();
+        request.setInstanceId(1L);
+
+        Mockito.when(productServiceMock.validateRequest(request)).thenReturn(null);
+        Mockito.when(agreementServiceMock.createAgreement(request))
+                .thenReturn(ProductResponse.getOkResponse("1"));
+
+        final ResponseEntity<ProductResponse> response = productController.postRequest(request);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Ошибка - возникновение исключения")
+    public void error_generateThrow() {
+        Mockito.when(productServiceMock.validateRequest(null)).thenThrow(IllegalArgumentException.class);
+
+        final ResponseEntity<ProductResponse> response = productController.postRequest(null);
+        Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+}
