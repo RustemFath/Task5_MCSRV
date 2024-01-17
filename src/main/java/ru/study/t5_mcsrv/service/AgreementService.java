69d02f0a6a3bc9ad066c8af3f1758bc43ed404a1
Фатхутдинов Рустем Римович <rfathutdinov@dev.vtb.ru>
2024-01-17 11:41:00 +0500

    Add logs AgreementService

--- src/main/java/ru/study/t5_mcsrv/service/AgreementService.java
+++ src/main/java/ru/study/t5_mcsrv/service/AgreementService.java
@@ -1,5 +1,7 @@
 package ru.study.t5_mcsrv.service;
 
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,8 @@ import java.util.List;
  * Сервис, создающий доп. соглашения
  */
 @Service
+@Setter
+@Slf4j
 public class AgreementService {
     @Autowired
     private ProductRepository productRepo;
@@ -29,26 +33,29 @@ public class AgreementService {
         // Проверка на существование договора с ID
         Product product = productRepo.findById(request.getContractId()).orElse(null);
         if (product == null) {
-            return ResponseMaker.getNotFoundResponse(new ProductResponse(),
-                    String.format("ЭП с ИД %d не найден в БД", request.getContractId()));
+            String info = String.format("ЭП с ИД %d не найден в БД", request.getContractId());
+            log.info(info);
+            return ResponseMaker.getNotFoundResponse(new ProductResponse(), info);
         }
 
         // Проверка таблицы ДС на дубли
         for (var dc : request.getInstanceAgreements()) {
             final List<Agreement> listDC = agreementRepo.findAgreementsByNumber(dc.getNumber());
             if (!listDC.isEmpty()) {
-                return ResponseMaker.getBadResponse(new ProductResponse(),
-                        String.format("Параметр № Дополнительного соглашения (сделки) Number %s уже существует для ЭП с ИД %d",
-                                dc.getNumber(), listDC.get(0).getProductId()));
+                String info = String.format("Параметр № Дополнительного соглашения (сделки) Number %s уже существует для ЭП с ИД %d",
+                        dc.getNumber(), listDC.get(0).getProductId());
+                log.info(info);
+                return ResponseMaker.getBadResponse(new ProductResponse(), info);
             }
         }
 
         // Проверка на дубли номеров во входящем массиве
         int uniqNumberSize = request.getInstanceAgreements().stream().distinct().toList().size();
         if (uniqNumberSize < request.getInstanceAgreements().size()) {
-            return ResponseMaker.getBadResponse(new ProductResponse(),
-                    String.format("Обнаружены дубли параметра № Дополнительного соглашения (сделки) Number для ЭП с ИД %d",
-                            request.getContractId()));
+            String info = String.format("Обнаружены дубли параметра № Дополнительного соглашения (сделки) Number для ЭП с ИД %d",
+                    request.getContractId());
+            log.info(info);
+            return ResponseMaker.getBadResponse(new ProductResponse(), info);
         }
 
         // создать массив новых доп соглашений
@@ -64,6 +71,7 @@ public class AgreementService {
         // Формирование ответа
         ProductResponse response = ResponseMaker.getOkResponse(new ProductResponse(), product.getId().toString());
         response.setSupplementaryAgreementId(agreements.stream().map(agr -> agr.getId().toString()).toList());
+        log.info("Доп соглашения созданы {}", response);
         return response;
     }
 }
